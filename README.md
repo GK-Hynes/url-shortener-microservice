f3e5178013430b3f7d6a6bf35d715d2bd11c8751
Gerard Hynes <gkhynes@gmail.com>
2021-01-08 16:31:54 +0000

    Generate short URL

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# [URL Shortener Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/url-shortener-microservice)
+# URL Shortener Microservice
+
+A URL shortener microservice built for the freeCodeCamp APIs and Microservices certificate.
+
+Requirements
+
+- You can POST a URL to `/api/shorturl/new` and get a JSON response with `original_url` and `short_url` properties. Here's an example: `{ original_url : 'https://freeCodeCamp.org', short_url : 1}`
+
+- When you visit `/api/shorturl/<short_url>`, you will be redirected to the original URL.
+
+- If you pass an invalid URL that doesn't follow the valid `<http://www.example.com>` format, the JSON response will contain `{ error: 'invalid url' }`
--- server.js
+++ server.js
@@ -1,24 +1,42 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require("dotenv").config();
+const crypto = require("crypto");
+const dns = require("dns");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// Basic Configuration
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl/new", (req, res) => {
+  // Check if passed URL is valid URL
+  const originalUrl = req.body.url;
+  const urlRegex = /^https?:\/\//i;
+  const hostname = originalUrl.replace(urlRegex, "");
+
+  dns.lookup(hostname, (err, address, family) => {
+    if (err === null) {
+      const shortUrl = crypto.randomBytes(4).toString("hex");
+      res.json({ original_url: originalUrl, short_url: shortUrl });
+    } else {
+      console.log(err);
+    }
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", (req, res) => {
+  // Check for existing short URL
+  // Redirect to original URL
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
